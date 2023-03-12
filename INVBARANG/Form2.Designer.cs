b4dd6e7a1af6e328a7bdf86cb8671109dc6a183a
Ariela <arielalintang259@gmail.com>
2023-03-12 16:07:16 +0700

    Add project files.

---
+++ INVBARANG/Form2.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace INVBARANG
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.NB = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.KTG = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.H = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TK = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(472, 61);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(664, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Letters for Learners", 10F);
+            this.label1.Location = new System.Drawing.Point(395, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SEARCH";
+            // 
+            // NB
+            // 
+            this.NB.Location = new System.Drawing.Point(14, 120);
+            this.NB.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.NB.Name = "NB";
+            this.NB.Size = new System.Drawing.Size(321, 26);
+            this.NB.TabIndex = 0;
+            this.NB.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Letters for Learners", 10F);
+            this.label2.Location = new System.Drawing.Point(14, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NAMA BARANG";
+            // 
+            // KTG
+            // 
+            this.KTG.Location = new System.Drawing.Point(14, 186);
+            this.KTG.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.KTG.Name = "KTG";
+            this.KTG.Size = new System.Drawing.Size(321, 26);
+            this.KTG.TabIndex = 0;
+            this.KTG.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Letters for Learners", 10F);
+            this.label3.Location = new System.Drawing.Point(14, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 21);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "KATEGORI";
+            // 
+            // H
+            // 
+            this.H.Location = new System.Drawing.Point(14, 249);
+            this.H.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.H.Name = "H";
+            this.H.Size = new System.Drawing.Size(321, 26);
+            this.H.TabIndex = 0;
+            this.H.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Letters for Learners", 10F);
+            this.label4.Location = new System.Drawing.Point(14, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 21);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "HARGA";
+            // 
+            // TK
+            // 
+            this.TK.Location = new System.Drawing.Point(14, 310);
+            this.TK.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.TK.Name = "TK";
+            this.TK.Size = new System.Drawing.Size(321, 26);
+            this.TK.TabIndex = 0;
+            this.TK.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Letters for Learners", 10F);
+            this.label5.Location = new System.Drawing.Point(14, 286);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(172, 21);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "TANGGAL KEDALUARSA";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(376, 96);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(847, 441);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // ID
+            // 
+            this.ID.Location = new System.Drawing.Point(17, 58);
+            this.ID.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.ID.Name = "ID";
+            this.ID.Size = new System.Drawing.Size(321, 26);
+            this.ID.TabIndex = 0;
+            this.ID.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Letters for Learners", 10F);
+            this.label6.Location = new System.Drawing.Point(17, 34);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 21);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "ID BARANG";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Orange;
+            this.button1.Font = new System.Drawing.Font("Letters for Learners", 12F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(492, 557);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(176, 68);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "CREATE";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkOrange;
+            this.button2.Font = new System.Drawing.Font("Letters for Learners", 12F);
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(717, 557);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 68);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "DELETE";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkGoldenrod;
+            this.button3.Font = new System.Drawing.Font("Letters for Learners", 12F);
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(942, 557);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 68);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "UPDATE";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Chocolate;
+            this.button4.Font = new System.Drawing.Font("Letters for Learners", 12F);
+            this.button4.Location = new System.Drawing.Point(90, 584);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(127, 60);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "LOAD";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(14, 371);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(294, 196);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(1166, 61);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(48, 26);
+            this.textBox2.TabIndex = 0;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSalmon;
+            this.ClientSize = new System.Drawing.Size(1236, 651);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TK);
+            this.Controls.Add(this.H);
+            this.Controls.Add(this.KTG);
+            this.Controls.Add(this.ID);
+            this.Controls.Add(this.NB);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox NB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox KTG;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox H;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TK;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox ID;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ INVBARANG/Form2.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace INVBARANG
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+
+        }
+        MySqlConnection connection = new MySqlConnection("server=localhost;user=root;password=;database=db_gg");
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            Tampil(textBox1.Text);
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Tampil("");
+        }
+
+        private void Tampil(string valueToSearch)
+        {
+            MySqlCommand command = new MySqlCommand("select * from master where concat(id, nama_barang, kategori, harga, exp) like '%" + valueToSearch + "%'", connection);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+            DataTable table = new DataTable();
+            adapter.Fill(table);
+            dataGridView1.RowTemplate.Height = 60;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.DataSource = table;
+            DataGridViewImageColumn imgcol = new DataGridViewImageColumn();
+            imgcol = (DataGridViewImageColumn)dataGridView1.Columns[5];
+            imgcol.ImageLayout = DataGridViewImageCellLayout.Stretch;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.Columns[0].HeaderText = "ID";
+            dataGridView1.Columns[1].HeaderText = "Nama BARANG";
+            dataGridView1.Columns[2].HeaderText = "Kategori";
+            dataGridView1.Columns[3].HeaderText = "Harga";
+            dataGridView1.Columns[4].HeaderText = "Tanggal Kedaluarsa";
+            dataGridView1.Columns[5].HeaderText = "Foto";
+        }
+        public void ExecMyQuery(MySqlCommand mcomd, string myMsg)
+        {
+            connection.Open();
+            if (mcomd.ExecuteNonQuery() == 1)
+            {
+                MessageBox.Show(myMsg);
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+            connection.Close();
+            Tampil("");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opf = new OpenFileDialog();
+            opf.Filter = "Choose Image(*.JPG;*.PNG;*.JPEG;*.GIF;)|*.jpg;*.png;*.jpeg;*.gif;";
+            if (opf.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(opf.FileName);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MemoryStream ms = new MemoryStream();
+            pictureBox1.Image.Save(ms, pictureBox1.Image.RawFormat);
+            byte[] img = ms.ToArray();
+            MySqlCommand command = new MySqlCommand("INSERT INTO master(nama_barang, kategori, harga, exp, foto) VALUES (@nama_barang,@kategori,@harga,@exp,@foto)", connection);
+            command.Parameters.Add("@nama_barang", MySqlDbType.VarChar).Value = NB.Text;
+            command.Parameters.Add("@kategori", MySqlDbType.VarChar).Value = KTG.Text;
+            command.Parameters.Add("@harga", MySqlDbType.VarChar).Value = H.Text;
+            command.Parameters.Add("@exp", MySqlDbType.VarChar).Value = TK.Text;
+            command.Parameters.Add("@foto", MySqlDbType.Blob).Value = img;
+
+            ExecMyQuery(command, "Data Berhasil Ditambahkan");
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            Byte[] img = (Byte[])dataGridView1.CurrentRow.Cells[7].Value;
+            MemoryStream ms = new MemoryStream(img);
+            pictureBox1.Image = Image.FromStream(ms);
+
+            ID.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            NB.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            KTG.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            H.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            TK.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MemoryStream ms = new MemoryStream();
+            pictureBox1.Image.Save(ms, pictureBox1.Image.RawFormat);
+            byte[] img = ms.ToArray();
+            MySqlCommand command = new MySqlCommand("UPDATE master SET nama_barang=@nama_barang,kategori=@kategori,harga=@harga,exp=@exp,foto=@foto WHERE id=@id", connection);
+            command.Parameters.Add("@id", MySqlDbType.VarChar).Value = ID.Text;
+            command.Parameters.Add("@nama_barang", MySqlDbType.VarChar).Value = NB.Text;
+            command.Parameters.Add("@kategori", MySqlDbType.VarChar).Value = KTG.Text;
+            command.Parameters.Add("@harga", MySqlDbType.VarChar).Value = H.Text;
+            command.Parameters.Add("@exp", MySqlDbType.VarChar).Value = TK.Text;
+            command.Parameters.Add("@foto", MySqlDbType.Blob).Value = img;
+
+            ExecMyQuery(command, "Data Berhasil Diubah");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MySqlCommand command = new MySqlCommand("DELETE FROM master WHERE id=@id", connection);
+            command.Parameters.Add("@id", MySqlDbType.VarChar).Value = textBox2.Text;
+
+            ExecMyQuery(command, "Data Berhasil Dihapus");
+        }
+    }
+}
